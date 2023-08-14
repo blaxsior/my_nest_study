b12d6995e509d73c67e36a89ba37cb31ed9cb60c
blaxsior <heejun2798@gmail.com>
2023-08-15 00:16:12 +0900

    feat: 유저 생성 api 기능 추가
    - CreateUserDto 추가
    - UserController 및 UserService에 생성 관련 메서드 추가

--- capp/src/users/users.controller.ts
+++ capp/src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto) {
+    await this.usersService.create(body.email, body.password);
+    return body;
+  }
+
+  @Get()
+  hello() {
+    console.log('hello');
+    return 'hello';
+  }
+}
